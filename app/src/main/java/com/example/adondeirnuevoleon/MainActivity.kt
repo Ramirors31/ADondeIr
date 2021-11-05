e16e1ded8f4099b5421a73bf23b15158296e0e12
AndresJDLRmr <90889489+AndresJDLRmr@users.noreply.github.com>
2021-11-05 12:02:55 -0600

    cambio de colores, se agrego splashscreen y el icono de la app

--- app/src/main/java/com/example/adondeirnuevoleon/MainActivity.kt
+++ app/src/main/java/com/example/adondeirnuevoleon/MainActivity.kt
@@ -7,6 +7,7 @@ import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        setTheme(R.style.Theme_ADondeIrNuevoLeon)
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val loginBtn = findViewById<Button>(R.id.loginBtn)
